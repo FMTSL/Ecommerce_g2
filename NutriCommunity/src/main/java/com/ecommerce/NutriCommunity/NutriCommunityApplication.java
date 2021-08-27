4f998e50f27ee44bd846a17e8c5f568c73a686dc
turma30java <felipe_matos@hotmail.com.br>
2021-08-27 09:57:40 -0300

    Spring

---
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/NutriCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.NutriCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriCommunityApplication.class, args);
+	}
+
+}
---
+++ NutriCommunity/src/test/java/com/ecommerce/NutriCommunity/NutriCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.NutriCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
