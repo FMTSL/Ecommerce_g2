0d312389d14d279bfd1c8b09fc733d098ad386bd
FabioCampaner <fabiocsuzuki@gmail.com>
2021-09-02 10:35:24 -0300

    Controller e Repository do Produto

---
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/controller/ProdutoController.java
@@ -0,0 +1,59 @@
+package com.ecommerce.NutriCommunity.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.NutriCommunity.model.Produto;
+import com.ecommerce.NutriCommunity.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> findById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nomeProduto/{nomeProduto}")
+	public ResponseEntity<List<Produto>> findByNomeProduto(@PathVariable String nomeProduto){
+		return ResponseEntity.ok(repository.findAllByNomeProdutoContainingIgnoreCase(nomeProduto));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> postProduto(@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> putProduto(@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteProduto(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+}
---
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.NutriCommunity.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.NutriCommunity.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+}
