e070fa6dc61be54597f1d89e0e28ece05b769f7b
Gisiqueira96 <giovanna.penedos@outlook.com>
2021-09-10 10:44:42 -0300

    Adiciona Segurança e Service

---
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/service/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.ecommerce.NutriCommunity.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.NutriCommunity.model.UserLogin;
+import com.ecommerce.NutriCommunity.model.Usuario;
+import com.ecommerce.NutriCommunity.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Optional<Usuario> cadastrarUsuario (Usuario usuario){
+		Optional<Usuario> user = repository.findByEmail(usuario.getEmail());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			if(user.isPresent()) {
+				return Optional.ofNullable(null);
+		}
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+			return Optional.of(repository.save(usuario));
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByEmail(user.get().getEmail());
+			if(usuario.isPresent()) {
+				if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+					
+					String auth = user.get().getEmail() + ":" + user.get().getSenha();
+					byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+					String authHeader = "Basic " + new String(encodedAuth);
+					
+					user.get().setToken(authHeader);
+					user.get().setNome(usuario.get().getNome());
+					
+					return user;
+				}
+			}
+			
+			return null;
+	}
+}
