83c81cecef37f60c988ffb5aee13d78db17641cd
Letpmartins <leticiapmartins08@gmail.com>
2021-09-14 11:22:42 -0300

    Documentação com Swagger e deploy no Heroku

--- NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/NutriCommunityApplication.java
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/NutriCommunityApplication.java
@@ -2,9 +2,20 @@ package com.ecommerce.NutriCommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class NutriCommunityApplication {
+	
+	@GetMapping
+	public ModelAndView swaggerUI() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NutriCommunityApplication.class, args);
--- NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/service/UsuarioService.java
+++ NutriCommunity/src/main/java/com/ecommerce/NutriCommunity/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package com.ecommerce.NutriCommunity.service;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.NutriCommunity.model.UserLogin;
 import com.ecommerce.NutriCommunity.model.Usuario;
@@ -17,6 +20,10 @@ public class UsuarioService {
 
 	@Autowired
 	private UsuarioRepository repository;
+	
+	public List<Usuario> listarUsuarios() {
+		return repository.findAll();
+	}
 	
 	public Optional<Usuario> cadastrarUsuario (Usuario usuario){
 		Optional<Usuario> user = repository.findByEmail(usuario.getEmail());
@@ -47,5 +54,23 @@ public class UsuarioService {
 			}
 			
 			return null;
+	}
+	
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (repository.findById(usuario.getId()).isPresent()) {
+
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+
+			return Optional.of(repository.save(usuario));
+
+		} else {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);
+
+		}
 	}
 }
